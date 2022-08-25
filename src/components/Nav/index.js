3fc800df76de31a94d4d419dc8373fb80b222fc4
seanxmcdaniel <102200863+seanxmcdaniel@users.noreply.github.com>
2022-08-24 22:18:28 -0400

    updated nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,51 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers'
 
 function Nav() {
 
-    const categories = [
+    const [categories] = useState([
         {
             name: 'Projects',
             description: 'List of front end, back end, and full stack projects completed throughout my MSU coding bootcamp.'
+        },
+        {
+            name: 'Back-End Projects',
+            description: 'List of back-end projects created throughout my MSU coding bootcamp.'
         }
-    ];
+    ]);
 
-    function categorySelected(name) {
-        console.log(`${name} clicked`)
-    }
+    const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
     return (
-        <header className='flex-row'>
-            <h2>
-                <a href='/'>
-                    Sean McDaniel
-                </a>
-            </h2>
-            <nav>
-                <ul className='flex-row'>
-                    <li className='mx-2'>
-                        <a href="#about">
-                            About Me
-                        </a>
-                    </li>
-                    <li>
-                        <span>
-                            Contact
-                        </span>
-                    </li>
-                    {categories.map((category) => (
-                        <li
-                            className='mx-1'
-                            key={category.name}
-                        >
-                            <span onClick={() => categorySelected(category.name)} >
-                                {category.name}
-                            </span>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-        </header>
+      <header className="flex-row px-1">
+        <h2>
+            Sean McDaniel
+        </h2>
+        <nav>
+          <ul className="flex-row">
+            <li className="mx-2">
+              <a
+                href="#about"
+              >
+                About me
+              </a>
+            </li>
+            <li>
+              <span>Contact</span>
+            </li>
+            {categories.map((category) => (
+              <li className={`mx-1 ${
+                  currentCategory.name === category.name && 'navActive'
+                  }`} key={category.name}>
+                <span
+                  onClick={() => {
+                    setCurrentCategory(category)
+                  }}
+                >
+                  {capitalizeFirstLetter(category.name)}
+                </span>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
     );
-}
-
-export default Nav;
+  }
+  
+  export default Nav;
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
-import photo from ''
+import photo from '../../assets/projects/happeningplace.png'
 
 function Projects(props) {
 
